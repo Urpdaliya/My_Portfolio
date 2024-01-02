9a32d758d5a84724a4d8a786450c5247425f8e9b
Utsav Padaliya <114207299+Urpdaliya@users.noreply.github.com>
2024-01-02 22:39:03 +0530

    Add files via upload

---
+++ Wallet.jsx
@@ -0,0 +1,34 @@
+import {useState} from "react";
+import ABI from "./ABI.json";
+import Web3 from "web3";
+import './Wallet.css';
+
+const Wallet =({saveState})=>{
+      const [connected,setConnected]=useState(true);
+      //when we open this website on android then new button for metamask is disable
+      const isAndroid = /android/i.test(navigator.userAgent);
+      const init =async()=>{
+      try{
+        const web3 = new Web3(window.ethereum);
+        await window.ethereum.request({method:'eth_requestAccounts'});
+        const contract = new web3.eth.Contract(
+            ABI,
+            "0xE9532e234bf3eD847f764fa7B181301D7F6ec255"
+        );
+         setConnected(false);
+         saveState({web3:web3,contract:contract});
+      }catch(error){
+        alert("Please Install Metamask");
+      }
+        
+      }
+      return<>
+      <div className="header">
+      {isAndroid  && <button className="connectBTN">
+         <a href="https://metamask.app.link/dapp/sriche.netlify.app/">Click For Mobile</a>
+        </button>  } 
+       <button className="connectBTN" onClick={init} disabled={!connected}> {connected? "Connect Metamask":"Connected"}</button>
+      </div>
+      </>
+}
+export default Wallet;
